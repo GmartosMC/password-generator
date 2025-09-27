a5c6d2893a6dbe7feb4301d74199af25401af8a2
GmartosMC <184553607+GmartosMC@users.noreply.github.com>
2025-09-27 11:05:11 +0200

    feat: starting

---
+++ main.py
@@ -0,0 +1,13 @@
+# Generador de contraseñas seguras
+
+# Importamos las librerías necesarias
+import secrets # Para la aleatoriedad
+import string # Para los caracteres
+import flet as ft # Para la GUI
+
+# Función Principal
+def main():
+    long = imput("Type the number of characters Ej. 8")
+
+def generate_password(long):
+
