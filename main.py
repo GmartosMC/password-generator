44d80cede98192566815b6ec61aedbd70bf7ffa1
GmartosMC <184553607+GmartosMC@users.noreply.github.com>
2025-09-27 15:30:49 +0200

    feat: add Basic interface

--- main.py
+++ main.py
@@ -5,19 +5,40 @@ import secrets # Para la aleatoriedad
 import string # Para los caracteres
 import flet as ft # Para la GUI
 
-def main():
+# La caja que guarda la Password
+password_field = ft.TextField(
+        read_only=True,
+        width=400,
+        text_align=ft.TextAlign.CENTER,
+        text_style=ft.TextStyle(size=20, weight=ft.FontWeight.BOLD)
+    )
 
-    # Preguntamos al usuario cuantos caracteres quiere
-    long = int(input("Type the number of characters. Ej 8: "))
-    password = password_generation(long)
-    print(f"The password is: {password}")
+# Función Main
+def main(page: ft.Page):
+    
+    # El título
+    page.title = "Password Generator"
+    title = ft.Text("Password Generator", size=30, weight=ft.FontWeight.BOLD)
 
-def password_generation(long):
+    # Generamos el botón
+    button = ft.ElevatedButton("Generate Password", on_click=update_password)
+
+    # Añadimos a la interfaz todo para mostrarlo
+    page.add(title, password_field, button)
+
+# Función para actualizar la contraseña
+def update_password(e):
+    password_field.value = password_generation()
+    e.page.update() # Actualizar solo el cambio
+
+# La función para generar la contraseña aleatoria
+def password_generation(length = 8):
 
     # Guardamos en una variable todos los tipos de caracteres que necesitamos
     characters = string.ascii_letters + string.digits + string.punctuation
 
-    password = "".join(secrets.choice(characters) for i in range (long))
+    password = "".join(secrets.choice(characters) for i in range (length))
     return password
 
-main()
+# Llamamos al Main
+ft.app(target=main)
