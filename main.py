db865e4611b6f13c29304e6db6d65cc845393cae
GmartosMC <184553607+GmartosMC@users.noreply.github.com>
2025-09-27 16:20:25 +0200

    feat: add slider working

--- main.py
+++ main.py
@@ -8,30 +8,53 @@ import flet as ft # Para la GUI
 # La caja que guarda la Password
 password_field = ft.TextField(
         read_only=True,
-        width=400,
-        text_align=ft.TextAlign.CENTER,
-        text_style=ft.TextStyle(size=20, weight=ft.FontWeight.BOLD)
+        width=1000,
+        text_align=ft.TextAlign.LEFT,
+        text_style=ft.TextStyle(
+            size=17, 
+            weight=ft.FontWeight.BOLD,
+            font_family="serif"
+            )
     )
-
-# El slider
-slider = ft.Slider(min=8, max=64, divisions=56, label="{value}", value=12)
 
 # Función Main
 def main(page: ft.Page):
     
     # El título
     page.title = "Password Generator"
-    title = ft.Text("Password Generator", size=30, weight=ft.FontWeight.BOLD)
+    title = ft.Text(
+        "Password Generator",
+        size=30,
+        weight=ft.FontWeight.BOLD)
+
+    # Generamos el slider
+    slider = ft.Slider(
+        min=8,
+        max=64,
+        divisions=56,
+        label="{value}",
+        value=12,
+        on_change=update_password
+        )
+
 
     # Generamos el botón
     button = ft.ElevatedButton("Generate Password", on_click=update_password)
 
     # Añadimos a la interfaz todo para mostrarlo
-    page.add(title, password_field, button, slider)
+    page.add(
+        title,
+        password_field,
+        ft.Text("Password Lenght:", size=20, weight=ft.FontWeight.BOLD),
+        slider,
+        button
+        )
 
 # Función para actualizar la contraseña
 def update_password(e):
-    password_field.value = password_generation()
+    password_field.value = password_generation(
+        int (e.control.value)
+    )
     e.page.update() # Actualizar solo el cambio
 
 # La función para generar la contraseña aleatoria
