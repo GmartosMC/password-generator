4bf73f3b305f3e44b8903fee1bcdd418971117ea
GmartosMC <184553607+GmartosMC@users.noreply.github.com>
2025-09-27 14:06:56 +0200

    feat: add generation

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ import secrets # Para la aleatoriedad
 import string # Para los caracteres
 import flet as ft # Para la GUI
 
-# Guardamos en una variable todos los tipos de caracteres que necesitamos
-characters = string.ascii_letters + string.digits + string.punctuation
-print(characters)
+def main():
 
+    # Preguntamos al usuario cuantos caracteres quiere
+    long = int(input("Type the number of characters. Ej 8: "))
+    password = password_generation(long)
+    print(password)
+
+def password_generation(long):
+
+    # Guardamos en una variable todos los tipos de caracteres que necesitamos
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = "".join(secrets.choice(characters) for i in range (long))
+    return password
+
+main()
